f7bff8f97269c2ca45e2dfb6a0129b03f8fa05d8
Trung Dang <dangtrungps@gmail.com>
2024-06-10 10:27:33 +0700

    up

--- popup.js
+++ popup.js
@@ -1,22 +1,30 @@
 
 document.addEventListener('DOMContentLoaded', function () {
     // Lấy các phần tử từ DOM
-    var startButton = document.getElementById('startButton');
-    var stopButton = document.getElementById('stopButton');
-    var recordedAudio = document.getElementById('recordedAudio');
-    var capturedStream;
-    var mediaRecorder;
-    var recordedChunks = [];
-    var audioContext;
-    var sourceNode;
-    var destinationNode;
+    let startButton = document.getElementById('startButton');
+    let stopButton = document.getElementById('stopButton');
+    let recordedAudio = document.getElementById('recordedAudio');
+    let capturedStream;
+    let mediaRecorder;
+    let recordedChunks = [];
+    let audioContext;
+    let sourceNode;
+    let destinationNode;
+    let audioUrls = [];
+    let allRecordedChunks = [];
 
+
+    //Function Call API
     function sendAudioToApi(recordedChunks) {
+
+        const startTime = Date.now();
+        console.log("Start time", startTime);
+
         console.log(`Bắt đầu gửi dữ liệu đến API tại: ${new Date().toLocaleTimeString()}`);
 
-        var recordedBlob = new Blob(recordedChunks, { type: 'audio/webm' });
+        let recordedBlob = new Blob(recordedChunks, { type: 'audio/webm' });
 
-        var formData = new FormData();
+        let formData = new FormData();
         formData.append('file', recordedBlob, 'recordedAudio.webm');
 
         fetch('http://localhost:3000/transcribe', {
@@ -26,6 +34,10 @@ document.addEventListener('DOMContentLoaded', function () {
             .then(response => response.json())
             .then(data => {
                 // console.log('Transcription:', data);
+                const endTime = Date.now();
+                console.log("End time", endTime);
+                console.log("Total time ", endTime - startTime);
+
                 console.log(`API Response tại: ${new Date().toLocaleTimeString()}`, data);
 
                 // Đảm bảo content script đã được inject và sẵn sàng nhận tin nhắn
@@ -44,39 +56,7 @@ document.addEventListener('DOMContentLoaded', function () {
 
             .catch(error => console.error('Error:', error));
     }
-    function sendAudioToApiStop(recordedChunks) {
-        console.log(`Bắt đầu gửi Stop dữ liệu đến API tại: ${new Date().toLocaleTimeString()}`);
 
-        var recordedBlob = new Blob(recordedChunks, { type: 'audio/webm' });
-
-        var formData = new FormData();
-        formData.append('file', recordedBlob, 'recordedAudio.webm');
-
-        fetch('http://localhost:3000/transcribe', {
-            method: 'POST',
-            body: formData
-        })
-            .then(response => response.json())
-            .then(data => {
-                console.log('TranscriptionStop:', data);
-                console.log(`API Stop Response tại: ${new Date().toLocaleTimeString()}`, data);
-
-                // Đảm bảo content script đã được inject và sẵn sàng nhận tin nhắn
-                chrome.tabs.query({ active: true, currentWindow: true }, (tabs) => {
-                    if (tabs[0]) {
-                        chrome.tabs.sendMessage(tabs[0].id, { type: 'tStopranscription', data: data }, (response) => {
-                            if (chrome.runtime.lastError) {
-                                console.error(chrome.runtime.lastError.message);
-                            } else {
-                                console.log('Message sent to content script', response);
-                            }
-                        });
-                    }
-                });
-            })
-
-            .catch(error => console.error('Error:', error));
-    }
     // Lắng nghe sự kiện click của nút startButton
     startButton.addEventListener('click', function () {
         // Sử dụng API chrome.tabCapture để capture âm thanh từ tab hiện tại
@@ -90,8 +70,6 @@ document.addEventListener('DOMContentLoaded', function () {
             console.log(`Stream captured tại: ${new Date().toLocaleTimeString()}`);
 
             capturedStream = stream;
-            // recordedChunks = [];
-
             audioContext = new AudioContext();
             sourceNode = audioContext.createMediaStreamSource(capturedStream);
             sourceNode.connect(audioContext.destination);
@@ -103,70 +81,80 @@ document.addEventListener('DOMContentLoaded', function () {
             mediaRecorder.ondataavailable = function (e) {
                 if (e.data.size > 0) {
                     const newChunk = e.data;
-                    console.log(`Data available tại: ${new Date().toLocaleTimeString()}`);
                     recordedChunks.push(newChunk);
-                    sendAudioToApi(recordedChunks)
                 }
             };
 
             mediaRecorder.start();
 
             const requestDataInterval = setInterval(() => {
-                mediaRecorder.requestData();
+                if (mediaRecorder.state === "recording") {
+                    mediaRecorder.stop();
+
+                    mediaRecorder.onstop = function () {
+                        if (recordedChunks.length > 0) {
+                            // Call API 
+                            sendAudioToApi(recordedChunks);
+                            const recordedBlob = new Blob(recordedChunks, { type: 'audio/webm' });
+                            const recordedUrl = URL.createObjectURL(recordedBlob);
+
+                            // Lưu URL vào thẻ audio tương ứng
+                            if (recordingIndex < 3) {
+                                audioUrls[recordingIndex] = recordedUrl;
+                                audioElements[recordingIndex].src = recordedUrl;
+                            }
+                            recordedChunks = [];
+                            mediaRecorder.start();
+
+                            recordingIndex++;
+
+                            console.log(`Đã lưu đoạn ghi âm vào thẻ recordedAudio${recordingIndex} và phát lại: ${new Date().toLocaleTimeString()}`);
+                        } else {
+                            console.warn('Không có dữ liệu âm thanh nào được ghi lại.');
+                        }
+                    };
+                }
             }, 1000);
 
         });
     });
 
-
-
+    // Lắng nghe sự kiện click của nút StopBtn
 
     stopButton.addEventListener('click', function () {
+        console.log("DAAAA clikc stop btn");
         if (mediaRecorder && mediaRecorder.state !== "inactive") {
             mediaRecorder.stop();
 
-            // Xử lý dữ liệu cuối cùng khi ghi âm dừng
             mediaRecorder.onstop = function () {
-                console.log(`Recording stopped tại: ${new Date().toLocaleTimeString()}`);
-                if (recordedChunks.length > 0) {
-                    sendAudioToApiStop(recordedChunks);
+                if (allRecordedChunks.length > 0) {
+                    console.log(`Data available tại mediaRecorder.stop();: ${new Date().toLocaleTimeString()}`);
+                    console.log("allRecordedChunks ", allRecordedChunks);
+                    var recordedBlob = new Blob(allRecordedChunks, { type: 'audio/webm' });
+                    var reader = new FileReader();
+                    reader.onloadend = function () {
+                        var base64data = reader.result;
+                        chrome.storage.local.set({ 'recordedAudio': base64data }, function () {
+                            console.log('Đã lưu âm thanh vào bộ nhớ');
+                        });
+                        var recordedUrl = URL.createObjectURL(recordedBlob);
+                        recordedAudio.src = recordedUrl;
+                    };
+                    reader.readAsDataURL(recordedBlob);
+                } else {
+                    console.warn('Không có dữ liệu âm thanh nào được ghi lại.');
                 }
+                capturedStream.getTracks().forEach(track => track.stop());
+                audioContext.close();
             };
         }
     });
 
-
-    // stopButton.addEventListener('click', function () {
-    //     if (mediaRecorder && mediaRecorder.state !== "inactive") {
-    //         mediaRecorder.stop();
-
-    //         mediaRecorder.onstop = function () {
-    //             if (recordedChunks.length > 0) {
-    //                 var recordedBlob = new Blob(recordedChunks, { type: 'audio/webm' });
-    //                 var reader = new FileReader();
-    //                 reader.onloadend = function () {
-    //                     var base64data = reader.result;
-    //                     chrome.storage.local.set({ 'recordedAudio': base64data }, function () {
-    //                         console.log('Đã lưu âm thanh vào bộ nhớ');
-    //                     });
-    //                     var recordedUrl = URL.createObjectURL(recordedBlob);
-    //                     recordedAudio.src = recordedUrl;
-    //                 };
-    //                 reader.readAsDataURL(recordedBlob);
-    //             } else {
-    //                 console.warn('Không có dữ liệu âm thanh nào được ghi lại.');
-    //             }
-    //             stream.getTracks().forEach(track => track.stop());
-    //             audioContext.close();
-    //         };
-    //     }
-    // });
-
-    // chrome.storage.local.get('recordedAudio', function (data) {
-    //     if (data.recordedAudio) {
-    //         recordedAudio.src = data.recordedAudio;
-    //     }
-    // });
+    chrome.storage.local.get('recordedAudio', function (data) {
+        if (data.recordedAudio) {
+            recordedAudio.src = data.recordedAudio;
+        }
+    });
 });
 
 
